744e2ced340d1a8be243b4c3f9d50038e9b1ca03
aez <aez@email.com>
2026-06-16 19:04:32 +0530

    modify: locals.tf change mongo_url = mongo:6

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   ecr_base     = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com"
-  frontend_url = "${local.ecr_base}/dev-frontend:${var.image_tag}"
-  backend_url  = "${local.ecr_base}/dev-backend:${var.image_tag}"
-  mongo_url    = "${local.ecr_base}/dev-mongo:${var.image_tag}"
+  frontend_url = "${local.ecr_base}/${var.env}-frontend:${var.image_tag}"
+  backend_url  = "${local.ecr_base}/${var.env}-backend:${var.image_tag}"
+  mongo_url    = "mongo:6"
 }
